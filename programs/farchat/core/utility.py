7c6d1b04e989c98558649076c9e1848960df6d4f
eltertrias <eltertrias>
2022-02-08 00:00:54 +0200

    seperated the code into several folders

--- programs/farchat/core/utility.py
+++ programs/farchat/core/utility.py
@@ -8,3 +8,7 @@ import sys
 # @param exit_status : tells wether the program should return successfull or unsucessfull exit
 def clean_exit(exit_status):
 	sys.exit(exit_status) # close the program returning succesfull exit
+
+# @brief : prints debugging information and only when the debugging mode is enabled
+def print_debug():
+	pass
